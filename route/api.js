0eb1d9ce33f76b15af4f249ed23d116bc05ea9f8
viviaan09 <ngvivian682@gmail.com>
2025-10-25 14:49:19 +0700

    update api routes with auth and protect middleware

--- route/api.js
+++ route/api.js
@@ -5,12 +5,13 @@ import {protect} from "../middleware/authMiddleware.js"
 
 const api = express.Router()
 
-api.get('/feedback', feedbackController.listFeedback)
-api.post('/feedback', feedbackController.createNewFeedback)
-api.put('/feedback/:id', feedbackController.updateFeedback)
-api.delete('/feedback/:id', feedbackController.deleteFeedback)
+api.get('/feedback', protect, feedbackController.listFeedback)
+api.post('/feedback', protect, feedbackController.createNewFeedback)
+api.put('/feedback/:id', protect, feedbackController.updateFeedback)
+api.delete('/feedback/:id', protect, feedbackController.deleteFeedback)
 
 api.post('/register', authController.register);
 api.post('/login', authController.login);
+api.get('/profile', protect, authController.getProfile);
 
 export default api;
