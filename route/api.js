5edac1c91ac305ab4b49d713e8a8f831a09d2c62
viviaan09 <ngvivian682@gmail.com>
2025-10-25 14:18:01 +0700

    update api routes for auth endpoint

--- route/api.js
+++ route/api.js
@@ -1,5 +1,7 @@
 import express from "express"
 import * as feedbackController from "../controllers/feedbackController.js"
+import * as authController from "../controllers/authController.js"
+import {protect} from "../middleware/authMiddleware.js"
 
 const api = express.Router()
 
@@ -7,5 +9,8 @@ api.get('/feedback', feedbackController.listFeedback)
 api.post('/feedback', feedbackController.createNewFeedback)
 api.put('/feedback/:id', feedbackController.updateFeedback)
 api.delete('/feedback/:id', feedbackController.deleteFeedback)
+
+api.post('/register', authController.register);
+api.post('/login', authController.login);
 
 export default api;
